271feaa30a5b8791eb0926bfc44de81aa3164ebb
Jacob Jackson <jacobjackson1985@icloud.com>
2023-10-29 19:37:43 -0500

    Initial Commit

---
+++ myfirstswiftui/myfirstswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  myfirstswiftuiApp.swift
+//  myfirstswiftui
+//
+//  Created by Jacob Jackson on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct myfirstswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
